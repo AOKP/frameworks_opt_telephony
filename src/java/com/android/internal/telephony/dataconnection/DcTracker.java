be4f4c4212e64cebbabcfa7d282062d6d1422d9a
Shareef Ali <shareefalis@cyanogenmod.org>
2013-11-10 21:59:30 -0500

    Telephony: DcTracker: Fix CDMA APN Data issues.
    Fix CDMA data issue when it can't find an APN match. This fixes cases
    CDMA NV or RIUM/SIM doesn't have the information to pass on.
    It assumes GSM like mode on CDMA. So fix that and pass on to CDMA
    stubbed apn found in android 4.2.
    
    Change-Id: Ib4467dd955eb71cbc5781db965d34357b7d4d2d9
    
    Conflicts:
    	src/java/com/android/internal/telephony/dataconnection/DcTracker.java

--- src/java/com/android/internal/telephony/dataconnection/DcTracker.java
+++ src/java/com/android/internal/telephony/dataconnection/DcTracker.java
@@ -670,10 +670,9 @@ public final class DcTracker extends DcTrackerBase {
             }
             int radioTech = mPhone.getServiceState().getRilDataRadioTechnology();
             if (apnContext.getState() == DctConstants.State.IDLE) {
-
-                ArrayList<ApnSetting> waitingApns = buildWaitingApns(apnContext.getApnType(),
-                        radioTech);
-                if (waitingApns.isEmpty()) {
+                ArrayList<DataProfile> waitingDps =
+                        buildWaitingApns(apnContext.getDataProfileType(), radioTech);
+                if (waitingDps.isEmpty()) {
                     notifyOffApnsOfAvailability(apnContext.getReason());
                     retValue = setupData(apnContext, radioTech);
                     if(!retValue) {
